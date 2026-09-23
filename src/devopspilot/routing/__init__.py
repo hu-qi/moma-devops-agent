fd32fcfed004299bc05223071bdbfcb5123da70e
huqi <huqi1024@gmail.com>
2026-09-23 23:35:38 +0800

    feat: export delivery task profiler

--- src/devopspilot/routing/__init__.py
+++ src/devopspilot/routing/__init__.py
@@ -1,11 +1,13 @@
 """Model capability routing for DevOpsPilot."""
 
 from .policy import DefaultCapabilityPolicy
+from .profiler import DeliveryTaskProfiler
 from .roles import AgentTeamModelPlan, AgentTeamModelPlanner
 from .router import ModelRouter
 
 __all__ = [
     "DefaultCapabilityPolicy",
+    "DeliveryTaskProfiler",
     "ModelRouter",
     "AgentTeamModelPlan",
     "AgentTeamModelPlanner",
