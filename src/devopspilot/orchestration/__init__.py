ab2c85aa78b7a2efa6fd73315c84a5ade2304e63
huqi <huqi1024@gmail.com>
2026-09-23 23:29:12 +0800

    feat: export durable delivery orchestrator

--- src/devopspilot/orchestration/__init__.py
+++ src/devopspilot/orchestration/__init__.py
@@ -2,5 +2,6 @@
 
 from .delivery_loop import DeliveryLoop
 from .execution import PublishingTaskExecutor
+from .service import DeliveryOrchestrator
 
-__all__ = ["DeliveryLoop", "PublishingTaskExecutor"]
+__all__ = ["DeliveryLoop", "PublishingTaskExecutor", "DeliveryOrchestrator"]
