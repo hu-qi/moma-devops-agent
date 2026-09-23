dc12a63dc1e6eabcee1cb881a20d182cb8db5b79
huqi <huqi1024@gmail.com>
2026-09-24 02:11:53 +0800

    feat: export OpenJiuwen trajectory bridge

--- src/devopspilot/trajectory/__init__.py
+++ src/devopspilot/trajectory/__init__.py
@@ -1,5 +1,21 @@
-"""Trajectory recording for DevOpsPilot."""
+"""Canonical DevOpsPilot trajectory helpers."""
 
+from .openjiuwen_capture import (
+    DEFAULT_OPENJIUWEN_SPAN_CATEGORIES,
+    OpenJiuwenCaptureResult,
+    OpenJiuwenTrajectoryCapture,
+)
+from .openjiuwen_bridge import (
+    map_openjiuwen_span,
+    openjiuwen_to_delivery_trajectory,
+)
 from .recorder import InMemoryTrajectoryRecorder
 
-__all__ = ["InMemoryTrajectoryRecorder"]
+__all__ = [
+    "DEFAULT_OPENJIUWEN_SPAN_CATEGORIES",
+    "InMemoryTrajectoryRecorder",
+    "OpenJiuwenCaptureResult",
+    "OpenJiuwenTrajectoryCapture",
+    "map_openjiuwen_span",
+    "openjiuwen_to_delivery_trajectory",
+]
