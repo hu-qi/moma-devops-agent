0470bf227092d610decff173b0754676dbda32ef
huqi <huqi1024@gmail.com>
2026-09-23 23:20:42 +0800

    feat: export AgentTeam model planner

--- src/devopspilot/routing/__init__.py
+++ src/devopspilot/routing/__init__.py
@@ -1,6 +1,12 @@
 """Model capability routing for DevOpsPilot."""
 
 from .policy import DefaultCapabilityPolicy
+from .roles import AgentTeamModelPlan, AgentTeamModelPlanner
 from .router import ModelRouter
 
-__all__ = ["DefaultCapabilityPolicy", "ModelRouter"]
+__all__ = [
+    "DefaultCapabilityPolicy",
+    "ModelRouter",
+    "AgentTeamModelPlan",
+    "AgentTeamModelPlanner",
+]
