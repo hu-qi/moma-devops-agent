d6b65f7d837ec0eaca99a6b5d48bcd4e33dd0836
huqi <huqi1024@gmail.com>
2026-09-24 03:20:15 +0800

    feat: export sandbox candidate materializer

--- src/devopspilot/adapters/openjiuwen/__init__.py
+++ src/devopspilot/adapters/openjiuwen/__init__.py
@@ -1,9 +1,10 @@
 """OpenJiuwen runtime adapters for DevOpsPilot."""
 
-from .evolution import OpenJiuwenSkillEvolutionProvider
+from .evolution import OpenJiuwenSkillEvolutionProvider, materialize_skill_experience_candidate
 from .executor import OpenJiuwenTaskExecutor
 
 __all__ = [
     "OpenJiuwenTaskExecutor",
     "OpenJiuwenSkillEvolutionProvider",
+    "materialize_skill_experience_candidate",
 ]
