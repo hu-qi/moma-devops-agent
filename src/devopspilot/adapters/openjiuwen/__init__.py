af088fb360021a019e44e93bf352ffe967d39eda
huqi <huqi1024@gmail.com>
2026-09-24 03:15:07 +0800

    feat: export OpenJiuwen evolution provider

--- src/devopspilot/adapters/openjiuwen/__init__.py
+++ src/devopspilot/adapters/openjiuwen/__init__.py
@@ -1,10 +1,9 @@
 """OpenJiuwen runtime adapters for DevOpsPilot."""
 
+from .evolution import OpenJiuwenSkillEvolutionProvider
 from .executor import OpenJiuwenTaskExecutor
-from .model_router import OpenJiuwenTeamModelRouting, build_team_model_routing
 
 __all__ = [
     "OpenJiuwenTaskExecutor",
-    "OpenJiuwenTeamModelRouting",
-    "build_team_model_routing",
+    "OpenJiuwenSkillEvolutionProvider",
 ]
