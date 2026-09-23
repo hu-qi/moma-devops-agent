c6ad16cc696b99b1cc7de060651e365db060212f
huqi <huqi1024@gmail.com>
2026-09-23 22:59:25 +0800

    feat: export CNB providers

--- src/devopspilot/adapters/cnb/__init__.py
+++ src/devopspilot/adapters/cnb/__init__.py
@@ -1,5 +1,11 @@
-"""CNB adapter primitives."""
+"""CNB provider adapters."""
 
-from .client import CNBCLIClient, CNBCLIError
+from .ci import CNBCIProvider
+from .client import CNBAPIClient, CNBAPIError, CNBCLIClient, CNBCLIError, CNBHTTPClient
+from .scm import CNBSCMProvider
 
-__all__ = ["CNBCLIClient", "CNBCLIError"]
+__all__ = [
+    "CNBAPIClient", "CNBAPIError", "CNBHTTPClient",
+    "CNBCLIClient", "CNBCLIError",
+    "CNBSCMProvider", "CNBCIProvider",
+]
