773fab72ed6704480e539d59647c27dc45d856e9
huqi <huqi1024@gmail.com>
2026-09-25 07:38:08 +0800

    feat: export autonomous delivery control plane

--- src/devopspilot/orchestration/__init__.py
+++ src/devopspilot/orchestration/__init__.py
@@ -1,7 +1,21 @@
 """Provider-neutral DevOpsPilot orchestration."""
 
+from .control_plane import (
+    AutonomousDeliveryControlPlane,
+    BoundedRemediationPolicy,
+    RuleBasedCIFailureAnalyzer,
+)
+from .control_plane_service import AutonomousDeliveryOrchestrator
 from .delivery_loop import DeliveryLoop
 from .execution import PublishingTaskExecutor
 from .service import DeliveryOrchestrator
 
-__all__ = ["DeliveryLoop", "PublishingTaskExecutor", "DeliveryOrchestrator"]
+__all__ = [
+    "DeliveryLoop",
+    "PublishingTaskExecutor",
+    "DeliveryOrchestrator",
+    "AutonomousDeliveryControlPlane",
+    "AutonomousDeliveryOrchestrator",
+    "BoundedRemediationPolicy",
+    "RuleBasedCIFailureAnalyzer",
+]
