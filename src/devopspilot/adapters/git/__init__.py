a31ed18fd479a982ce06da328d946a7b59043f34
huqi <huqi1024@gmail.com>
2026-09-25 07:42:04 +0800

    feat: export existing branch workspace

--- src/devopspilot/adapters/git/__init__.py
+++ src/devopspilot/adapters/git/__init__.py
@@ -1,6 +1,11 @@
 """Generic Git transport primitives."""
 
+from .existing_branch import GitExistingBranchWorkspaceProvider
 from .publisher import GitChangePublisher
 from .workspace import GitWorktreeWorkspaceProvider
 
-__all__ = ["GitChangePublisher", "GitWorktreeWorkspaceProvider"]
+__all__ = [
+    "GitChangePublisher",
+    "GitWorktreeWorkspaceProvider",
+    "GitExistingBranchWorkspaceProvider",
+]
